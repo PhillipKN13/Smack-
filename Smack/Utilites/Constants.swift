56eaf961a8585e9bae1e6e5914ad34ce15076ffd
Phillip Nguyen <phillip77478@gmail.com>
2018-01-11 22:50:20 -0600

    Login UI

---
+++ Smack/Utilites/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Phillip Nguyen on 1/11/18.
+//  Copyright © 2018 Phillip Nguyen. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
+
