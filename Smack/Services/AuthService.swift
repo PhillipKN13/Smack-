d31d601aa6ff70662fe0fd36013731e2193d0ad7
Phillip Nguyen <phillip77478@gmail.com>
2018-01-13 22:39:36 -0600

    log in user

--- Smack/Services/AuthService.swift
+++ Smack/Services/AuthService.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 //used for log in, create user, register user funcs, vars stored in user defaults
 
@@ -47,25 +48,53 @@ class AuthService {
     func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
         
         let lowerCaseEmail = email.lowercased()
-        
-        let header = [
-            "Content-Type": "application/json; charset=utf-8"
-        ]
-        
         let body: [String: Any] = [
             "email": lowerCaseEmail,
             "password": password
         ]
         
-        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default , headers: header).responseString { (response) in
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default , headers: HEADER).responseString { (response) in
             
-            if response.result.error == nil {
+            if response.result.error == nil { //API doesn't really send back any data to use like in loginUser
                 completion(true)
             } else {
                 completion(false)
                 debugPrint(response.result.error as Any)
             }
         }
+    }
+    
+    func loginUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        
+        let lowerCaseEmail = email.lowercased()
+        
+        let body: [String: Any] = [ //dict
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON { (response) in
+            
+            if response.result.error == nil {
+                guard let data = response.data else { return }
+                do {  //add do/catch bc swiftyJSON initializer throws
+                let json = try JSON(data: data)
+                self.userEmail = json["user"].stringValue  //.stringValue safely automatically unwraps the val for you OR it'll set it to an empty string
+                self.authToken = json["token"].stringValue
+                
+                self.isLoggedIn = true 
+                completion(true)
+                } catch {
+                    print(error)
+                }
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+        
+        
+        
     }
     
     
